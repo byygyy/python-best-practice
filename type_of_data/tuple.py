019509bb75b9d7c0448cdc65bca7e30231a0bee6
robin <lhh_nj@163.com>
2020-12-12 22:13:36 +0800

    add new

--- type_of_data/tuple.py
+++ type_of_data/tuple.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python
 # coding=utf-8
 
-names = ['robin', 'joe', 'john'];
+# tuple item cannt been changed
+names = ('robin', 'joe', 'john');
+print(names);
+# loop a tuple
+for name in names:
+    print(name);
+
+# but tuple can been changed
+names = ('jason','ivy')
+print(names)
+for name in names:
+    print(name);
