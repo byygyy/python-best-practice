0236ccad95d54fe262f95a27b39de4d7273537af
robin <lhh_nj@163.com>
2020-12-12 23:07:29 +0800

    modify

--- type_of_data/dict.py
+++ type_of_data/dict.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python
 # coding=utf-8
 
+# a simply dict and access it
+guys_0 = {"name": "robin", "age": 22};
+print(guys_0);
+print(guys_0["name"]);
+print(guys_0["age"]);
+
+# add new item for dict
+guys_0["city"] = "xi'an";
+print(guys_0);
+
+# create a empty dict and add new item
+guys_1 = {};
+guys_1["name"] = "jack";
+print(guys_1)
+
+# modify the key:value
+guys_0["city"] = 'shanghai';
+print(guys_0)
